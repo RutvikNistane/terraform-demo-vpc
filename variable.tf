9d056834ed533eac7050aa486951c01df79493b7
RutvikNistane <67835549+RutvikNistane@users.noreply.github.com>
2022-09-06 19:11:31 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "main_vpc_cidr" {
+}
+
+variable "public_subnets" {
+}
+
+variable "private_subnets" {
+}
+
+
+variable "access_key" {
+        type = string
+}
+
+variable "secret_key" {
+        type = string
+}
